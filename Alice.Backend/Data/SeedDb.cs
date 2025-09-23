57b5216b375837f755d6ffe669a5d9b756b4a212
SantiCastro-Lab <scastro666@outlook.com>
2025-09-22 23:01:48 -0500

    Add Seeder to Database

---
+++ Alice.Backend/Data/SeedDb.cs
@@ -0,0 +1,40 @@
+﻿using Alice.Shared.Entities;
+
+namespace Alice.Backend.Data;
+
+public class SeedDb
+{
+    private readonly DataContext _context;
+
+    public SeedDb(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+        await CheckCountriesAsync();
+        await CheckCategoriesAsync();
+    }
+
+    private async Task CheckCategoriesAsync()
+    {
+        if (!_context.Countries.Any())
+        {
+            _context.Countries.Add(new Country { Name = "Colombia" });
+            _context.Countries.Add(new Country { Name = "Mexico" });
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    private async Task CheckCountriesAsync()
+    {
+        if (!_context.Categories.Any())
+        {
+            _context.Categories.Add(new Category { Name = "Sueros" });
+            _context.Categories.Add(new Category { Name = "Ropa y Fajas" });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
