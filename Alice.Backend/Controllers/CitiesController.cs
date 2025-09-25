014ebb84e8482b76644e6411f8a3ae2f9974fdb2
SantiCastro-Lab <scastro666@outlook.com>
2025-09-25 16:20:19 -0500

    Add Cities Controller

---
+++ Alice.Backend/Controllers/CitiesController.cs
@@ -0,0 +1,15 @@
+﻿using Alice.Backend.UnitOfWork.Implementations;
+using Alice.Backend.UnitOfWork.Interfaces;
+using Alice.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alice.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CitiesController : GenericController<City>
+{
+    public CitiesController(IGenericUnitOfWork<City> unitOfWork) : base(unitOfWork)
+    {
+    }
+}
